75aa9cb539e5584a616791a422d62e0c4dadcace
Andrey Sokolov <falconandy@yandex.ru>
2018-05-17 20:48:46 +0400

    Minor fixes + up tests

--- backlog/links.go
+++ backlog/links.go
@@ -5,9 +5,17 @@ import (
 )
 
 func MakeItemLink(item *BacklogItem, baseDir string) string {
-	return utils.MakeMarkdownLink(item.Title(), item.markdown.contentPath, baseDir)
+	itemPath := item.markdown.contentPath
+	if itemPath == "" {
+		itemPath = item.Name()
+	}
+	return utils.MakeMarkdownLink(item.Title(), itemPath, baseDir)
 }
 
 func MakeIdeaLink(idea *BacklogIdea, baseDir string) string {
-	return utils.MakeMarkdownLink(idea.Title(), idea.markdown.contentPath, baseDir)
+	ideaPath := idea.markdown.contentPath
+	if ideaPath == "" {
+		ideaPath = idea.Name()
+	}
+	return utils.MakeMarkdownLink(idea.Title(), ideaPath, baseDir)
 }
