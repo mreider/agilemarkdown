240120864926863b040d7e7c4d5e2432531364b8
Andrey Sokolov <falconandy@yandex.ru>
2018-05-23 20:53:34 +0400

    A new 'archive' command to archive stories before a certain date

---
+++ commands/archive_command.go
@@ -0,0 +1,53 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/mreider/agilemarkdown/backlog"
+	"gopkg.in/urfave/cli.v1"
+	"path/filepath"
+	"time"
+)
+
+var ArchiveCommand = cli.Command{
+	Name:      "archive",
+	Usage:     "Archive stories before a certain date",
+	ArgsUsage: "YYYY-MM-DD",
+	Action: func(c *cli.Context) error {
+		if err := checkIsBacklogDirectory(); err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		if c.NArg() != 1 {
+			fmt.Println("A date should be specified")
+			return nil
+		}
+
+		beforeDate, err := time.Parse("2006-1-2", c.Args()[0])
+		if err != nil {
+			fmt.Println("Invalid date. Should be in YYYY-MM-DD format.")
+			return nil
+		}
+
+		backlogDir, _ := filepath.Abs(".")
+		bck, err := backlog.LoadBacklog(backlogDir)
+		if err != nil {
+			return err
+		}
+
+		var itemsToArchive []*backlog.BacklogItem
+		for _, item := range bck.ActiveItems() {
+			if item.Created().Before(beforeDate) {
+				itemsToArchive = append(itemsToArchive, item)
+			}
+		}
+
+		for _, item := range itemsToArchive {
+			item.SetArchived(true)
+			err := item.Save()
+			if err != nil {
+				fmt.Printf("Can't archive the item '%s': %v\n", item.Title(), err)
+			}
+		}
+		return nil
+	},
+}
