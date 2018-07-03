20f1fdbb9128d38f0556ed05214db2f43f624d6d
Andrey Sokolov <falconandy@yandex.ru>
2018-07-03 15:00:50 +0400

    || -> •

--- backlog/markdown.go
+++ backlog/markdown.go
@@ -16,7 +16,7 @@ const (
 )
 
 var (
-	linksRe = regexp.MustCompile(`^(\[[^])]+]\([^])]+\)(\s*(\|\|)?\s*)?)+$`)
+	linksRe = regexp.MustCompile(`^(\[[^])]+]\([^])]+\)(\s*(•)?\s*)?)+$`)
 )
 
 type MarkdownContent struct {
